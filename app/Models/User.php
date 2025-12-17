686930da18e35afa5fe9e5be708a2f8bbce6906f
tt1989182-cyber <tt1989182@gmail.com>
2025-12-17 21:40:42 +0700

    Refactor User model to include relationships and properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,50 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $table = 'users';
+
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'profile_image',
+        'bio',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    // Quan hệ với Post
+    public function posts(): HasMany
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Post::class);
+    }
+
+    // Quan hệ với Like
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    // Quan hệ với Comment
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
